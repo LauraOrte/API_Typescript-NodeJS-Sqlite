2a54c728fc2698d0d3b10f6b1d7702beaae5e900
LauraOrte <laura.orte7@gmail.com>
2022-06-21 19:38:38 +0200

    inicio proyecto

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import bodyParser from 'body-parser';
+import express, { Request, Response } from 'express';
+
+const PORT = process.env.POR || 8080;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/users', (req: Request, res: Response)=>{
+    res.json({
+        msg: 'Todo ok'
+    })
+})
+
+app.post('/users', (req: Request, res: Response)=>{
+    console.log(req.body);
+    res.json({
+        msg: 'Todo ok'
+    })
+}) 
+
+app.listen(PORT, () => console.log('Servidor inicializado en el puerto ' + PORT));
+
